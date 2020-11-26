477b508fbed3b59ca4ee2bc34d3bc719a4f12ae9
HarshVardhan22 <hsinha31@gmail.com>
2020-11-26 22:25:25 +0530

    fixed useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useContext,useState} from "react";
+import React,{useContext} from "react";
 import Navbar from "./components/Navbar";
 import Herosection from "./components/Herosection";
 import "./App.css";
