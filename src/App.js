b18af1c4f77eaa0fbbe6be14c5fbb14221cfbedd
HarshVardhan22 <hsinha31@gmail.com>
2020-11-26 20:09:17 +0530

    beta v1.2 responsive Now!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Herosection from "./components/Herosection"
-
+import "./App.css"
 function App() {
   return (
     <div className="App">
         <Navbar></Navbar>
         <Herosection/>
-     
+        <div className="themeSwitch">
+        <label className="switch">
+         <input type="checkbox"/>
+         <span className="slider round"></span>
+         </label> 
+         </div>
     </div>
   );
 }
